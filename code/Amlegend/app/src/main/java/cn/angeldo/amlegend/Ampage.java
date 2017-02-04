008ddce1bdef95a28dc1608c4cd8e79f6635a9c6
lvjichuan <578178852@qq.com>
2017-02-04 17:40:08 +0800

    APP文件

---
+++ code/Amlegend/app/src/main/java/cn/angeldo/amlegend/Ampage.java
@@ -0,0 +1,55 @@
+package cn.angeldo.amlegend;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+public class Ampage extends Activity {
+    private SimpleDraweeView mytx;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
+        setContentView(R.layout.activity_ampage);
+
+        mytx = (SimpleDraweeView)findViewById(R.id.my_tx);
+
+
+    }
+    //退出调用
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            dialog_loginout();
+        }
+        return false;
+    }
+    //退出提示
+    protected void dialog_loginout() {
+        new AlertDialog.Builder(this)
+                .setTitle("喂！")
+                .setMessage("就这样离开吗？")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialoginterface, int i) {
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        android.os.Process.killProcess(android.os.Process
+                                .myPid());
+                    }
+                })
+                .setNegativeButton("取消",
+                        new android.content.DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                dialog.dismiss();
+                            }
+                        }).show();
+    }
+}
