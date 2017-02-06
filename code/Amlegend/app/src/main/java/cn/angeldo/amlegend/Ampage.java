37dd6b9605b2a249c5bcda93e20e4f2bfba11022
lvjichuan <578178852@qq.com>
2017-02-06 17:52:41 +0800

    调整布局

--- code/Amlegend/app/src/main/java/cn/angeldo/amlegend/Ampage.java
+++ code/Amlegend/app/src/main/java/cn/angeldo/amlegend/Ampage.java
@@ -6,12 +6,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 public class Ampage extends Activity {
     private SimpleDraweeView mytx;
+    private RelativeLayout b_area;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,30 @@ public class Ampage extends Activity {
         setContentView(R.layout.activity_ampage);
 
         mytx = (SimpleDraweeView)findViewById(R.id.my_tx);
+
+        b_area=(RelativeLayout)findViewById(R.id.boom_area);
+
+        try {
+            ImageView t1 = new ImageView(this);
+            t1.setImageResource(R.drawable.target);
+            //t1.setBackgroundResource(R.drawable.locking);
+            t1.setPadding(10,10,10,10);
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(80,80);
+            lp.topMargin=200;
+            lp.leftMargin=200;
+            t1.setLayoutParams(lp);
+            t1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    v.setBackgroundResource(R.drawable.locking);
+                }
+            });
+
+            b_area.addView(t1);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     }
