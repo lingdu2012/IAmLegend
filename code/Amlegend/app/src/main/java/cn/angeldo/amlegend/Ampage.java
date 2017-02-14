5a83ec297722d89a82370646d9917e6db98493b1
lvjichuan <578178852@qq.com>
2017-02-14 17:02:38 +0800

    文件调整

--- code/Amlegend/app/src/main/java/cn/angeldo/amlegend/Ampage.java
+++ code/Amlegend/app/src/main/java/cn/angeldo/amlegend/Ampage.java
@@ -11,73 +11,47 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
 
+import static cn.angeldo.amlegend.R.drawable.target;
+import static cn.angeldo.amlegend.R.id.itool;
+
 public class Ampage extends Activity {
-    private SimpleDraweeView mytx;
-    private ImageView itool;
-    private ImageView btn_search;
-    private RelativeLayout b_area;
-    private int ichoice=-1;//选择中目标的index
+    private SimpleDraweeView user_logo;//用户头像
+    private ImageView boom_tool;//道具
+    private ImageView btn_search;//搜索按钮
+    private RelativeLayout boom_area;//攻击区域
+    private int current_choice=-1;//选择中目标的index
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fresco.initialize(this);
         setContentView(R.layout.activity_ampage);
 
-        mytx = (SimpleDraweeView)findViewById(R.id.my_tx);
-        itool=(ImageView)findViewById(R.id.itool);
+        user_logo = (SimpleDraweeView)findViewById(R.id.my_tx);
+        boom_tool=(ImageView)findViewById(itool);
         btn_search=(ImageView)findViewById(R.id.btn_search);
-        b_area=(RelativeLayout)findViewById(R.id.boom_area);
+        boom_area=(RelativeLayout)findViewById(R.id.boom_area);
 
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                boom_area.removeAllViews();
+                boom_area.notify();
+                scanTarget(4);
+                Toast.makeText(getApplicationContext(), "扫描完毕", Toast.LENGTH_SHORT).show();
             }
         });
 
-        try {
-            for(int i=0;i<2;i++) {
-                ImageView t1 = new ImageView(this);
-                t1.setImageResource(R.drawable.target);
-                int dwl = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics());
-                t1.setPadding(3 * dwl, 3 * dwl, 3 * dwl, 3 * dwl);
-                RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(25 * dwl, 25 * dwl);
-                lp.topMargin = (150 - 30*i) * dwl;
-                lp.leftMargin = (150 - 30*i) * dwl;
-                t1.setLayoutParams(lp);
-                t1.setId(i);
-                t1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Log.i("此处的id是", "：" + v.getId() + "元素个数为：" + b_area.getChildCount());
-                        if(ichoice==v.getId()){
-                            v.setBackgroundResource(0);
-                            ichoice = -1;
-                        }else{
-                            if(ichoice > -1) {
-                                b_area.getChildAt(ichoice).setBackgroundResource(0);
-                            }
-                            v.setBackgroundResource(R.drawable.locking);
-                            ichoice=v.getId();
-                        }
-
-                    }
-                });
-                b_area.addView(t1);
-            }
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
+        scanTarget(2);
         //绑定道具点击事件
-        itool.setOnClickListener(new View.OnClickListener() {
+        boom_tool.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b_area.removeViewAt(0);
-                dialog_ko();
+                boomTarget();
             }
         });
 
@@ -88,6 +62,68 @@ public class Ampage extends Activity {
             dialog_loginout();
         }
         return false;
+    }
+    //扫描目标
+    public void scanTarget(int num){
+        try {
+            for(int i=0;i<num;i++) {
+                ImageView t1 = new ImageView(this);
+                t1.setImageResource(target);
+                int dwl = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics());
+                t1.setPadding(3 * dwl, 3 * dwl, 3 * dwl, 3 * dwl);
+                RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(25 * dwl, 25 * dwl);
+                lp.topMargin = (150 - 30*i) * dwl;
+                lp.leftMargin = (150 - 30*i) * dwl;
+                t1.setLayoutParams(lp);
+                t1.setId(100+i);
+                Log.i("添加的id是", "：" + i + "实际数为：" +(100+i));
+                t1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i("此处的id是", "：" + v.getId() + "元素个数为：" + boom_area.getChildCount());
+                        changeTarget(v.getId());
+                    }
+                });
+                boom_area.addView(t1);
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        current_choice=-1;
+    }  //消灭目标
+    private void boomTarget(){
+        Log.i("消灭的id是", "：" + current_choice);
+        int targetNum=boom_area.getChildCount();
+        for(int i=0;i<targetNum;i++){
+            Log.i("遍历的id是", "：" + boom_area.getChildAt(i).getId()+"位置id:"+i);
+
+            if (boom_area.getChildAt(i).getId() == current_choice) {
+                boom_area.removeViewAt(i);
+            }
+
+        }
+        boom_area.refreshDrawableState();
+        dialog_ko();
+        current_choice=-1;
+    }
+    //更改目标
+    private void changeTarget(int targetId){
+        int targetNum=boom_area.getChildCount();
+        for(int i=0;i<targetNum;i++){
+            if(boom_area.getChildAt(i).getId() == targetId){
+                if(current_choice == targetId){
+                    boom_area.getChildAt(i).setBackgroundResource(0);
+                    current_choice=-1;
+                }else {
+                    boom_area.getChildAt(i).setBackgroundResource(R.drawable.locking);
+                    current_choice=targetId;
+                }
+            }else{
+                boom_area.getChildAt(i).setBackgroundResource(0);
+            }
+        }
+        Log.i("锁定的id是", "：" + current_choice);
     }
     //退出提示
     protected void dialog_loginout() {
